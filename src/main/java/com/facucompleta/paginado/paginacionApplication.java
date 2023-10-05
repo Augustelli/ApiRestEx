8d5659aea4af6c1c982b934a248586654b3d32e1
Augusto_Mancuso <a.mancuso@alumno.um.edu.ar>
2023-10-04 23:26:07 -0300

    Primer commit

---
+++ src/main/java/com/facucompleta/paginado/paginacionApplication.java
@@ -0,0 +1,13 @@
+package com.facucompleta.paginado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class paginacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(paginacionApplication.class, args);
+		System.out.println("Funcionando");
+	}
+}
